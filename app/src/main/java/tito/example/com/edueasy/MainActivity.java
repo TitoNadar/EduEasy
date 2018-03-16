b6142dd444907987b36c5a91323f69531609b882
TitoNadar <titonadar06@gmail.com>
2018-03-17 04:56:11 +0530

    profile done

--- app/src/main/java/tito/example/com/edueasy/MainActivity.java
+++ app/src/main/java/tito/example/com/edueasy/MainActivity.java
@@ -1,5 +1,6 @@
 package tito.example.com.edueasy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -102,8 +103,10 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.nav_Profile) {
+            Intent intent=new Intent(MainActivity.this,Profile.class);
+            startActivity(intent);
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
